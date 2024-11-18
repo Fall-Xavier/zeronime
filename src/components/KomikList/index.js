43e78d4f1bfb94881c4f5f13fe0da5bb99ef64d7
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2024-11-18 09:33:45 +0700

    Create index.js

---
+++ src/components/KomikList/index.js
@@ -0,0 +1,11 @@
+import Update from "@/components/KomikList/Update"
+
+const KomikList = () => {
+    return (
+        <div>
+            <Update />
+        </div>
+    )
+}
+
+export default KomikList
