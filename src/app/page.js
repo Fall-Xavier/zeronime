cf110df93263f3ab34f9ca9a3f873b54a60d60ed
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2024-11-18 09:30:26 +0700

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Header from "@/components/Header"
+import KomikList from "@/components/KomikList"
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <KomikList />
+        </div>
+    )
+}
+
+export default Home
