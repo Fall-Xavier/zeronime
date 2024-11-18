f1106082b44a44e2089e63bbdd12c59e8d7e090b
Fall Xavier <84975017+Fall-Xavier@users.noreply.github.com>
2024-11-18 09:33:11 +0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+
+const Header = () => {
+    return (
+        <div>
+            <div className="flex flex-col items-center justify-center p-2">
+                <div className="flex text-4xl font-bold">
+                    <span className="">Zero</span>
+                    <span className="text-blue-600">Nime</span>
+                </div>
+                <span className="text-xs -mt-2">Baca Komik Bahasa Indonesia</span>
+            </div>
+        </div>
+    )
+}
+
+export default Header
